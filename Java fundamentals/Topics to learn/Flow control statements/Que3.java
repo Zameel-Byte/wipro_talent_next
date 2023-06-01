199030c33042c80aff966febeb35812223d83017
Zameel ali mohammed <zameelalimohammed282003@gmail.com>
2023-06-01 21:14:05 +0530

    Task

--- Java fundamentals/Topics to learn/Flow control statements/Que3.java
+++ Java fundamentals/Topics to learn/Flow control statements/Que3.java
@@ -1,5 +1,14 @@
 public class Que3 {
     public static void main(String[] args){
-
+        if (args.length == 0){
+            System.out.println("No value");
+        } else {
+            int len = args.length;
+            for (int i = 0; i <= len-1; i++){
+                System.out.print(args[i]);
+                System.out.print(",");
+            }
+            System.out.print("\b");
+        }
     }
 }
